93f957f6a122b0b8d66c6aa7d3754d58e76e9648
basarat <basaratali@gmail.com>
2022-02-01 12:22:05 +1100

    more

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const Home: NextPage = () => {
     <div className='grid grid-cols-3 gap-x-2 gap-y-3 grid-flow-row-dense'>
       <div className='bg-red-500 rounded-lg shadow-xl min-h-[50px]' />
       <div className='bg-orange-500 rounded-lg shadow-xl min-h-[50px] col-span-3' />
-      <div className='bg-yellow-500 rounded-lg shadow-xl min-h-[50px] row-span-2' />
+      <div className='bg-yellow-500 rounded-lg shadow-xl min-h-[50px] row-span-2 col-span-2' />
       <div className='bg-green-500 rounded-lg shadow-xl min-h-[50px]' />
       <div className='bg-teal-500 rounded-lg shadow-xl min-h-[50px]' />
       <div className='bg-blue-500 rounded-lg shadow-xl min-h-[50px]' />
